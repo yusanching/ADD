b39bc684811d217a5db88af0d8116c1fd70edf1b
sanching <sanchingyu@gmail.com>
2018-01-13 11:32:49 +0800

    TEST

---
+++ ADD/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ADD
+{
+    public class Class1
+    {
+        public int Add()
+        {
+            var x = 2;
+            var y = 3;
+            return x + y;
+        }
+    }
+}
---
+++ ADDTests/Class1Tests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ADD;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ADD.Tests
+{
+    [TestClass()]
+    public class Class1Tests
+    {
+        [TestMethod()]
+        public void AddTest()
+        {
+            Class1 mock = new Class1();
+            var result = mock.Add();
+
+            Assert.AreEqual(5, result);
+        }
+    }
+}
